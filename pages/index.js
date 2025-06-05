e3f5da85c395fc0e2899e07b67ce0088a9e9ff97
takaaki99 <takaaki.5.27@icloud.com>
2025-06-06 00:24:40 +0900

    トップページに投稿一覧へのリンクを追加

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-// pages/index.tsx
-
+import Link from "next/link";
 import Layout from "../components/Layout";
 
-export default function HomePage() {
+export default function Home() {
   return (
     <Layout>
-      <div style={{ padding: "2rem" }}>
+      <div style={{ padding: "2rem", textAlign: "center" }}>
         <h1>ようこそ、NikiViewへ！</h1>
-        <p>ここはあなたの匿名お買い物日記アプリです。</p>
+        <p>あなたの買ってよかった日記を残しましょう。</p>
+
+        <div style={{ marginTop: "2rem" }}>
+          <Link href="/post">
+            <button style={{ margin: "0.5rem" }}>日記を投稿</button>
+          </Link>
+          <Link href="/posts">
+            <button style={{ margin: "0.5rem" }}>投稿一覧を見る</button>
+          </Link>
+        </div>
       </div>
     </Layout>
   );
