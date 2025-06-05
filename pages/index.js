f9491d94cf46acbc061b9bc1ae551fbea18b54cf
takaaki99 <takaaki.5.27@icloud.com>
2025-06-05 23:53:22 +0900

    トップページにもヘッダーを追加

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+// pages/index.tsx
 
-export default function Home() {
-  return <h1>ようこそ、NikiViewへ！</h1>
+import Layout from "../components/Layout";
 
+export default function HomePage() {
+  return (
+    <Layout>
+      <div style={{ padding: "2rem" }}>
+        <h1>ようこそ、NikiViewへ！</h1>
+        <p>ここはあなたの匿名お買い物日記アプリです。</p>
+      </div>
+    </Layout>
+  );
 }
